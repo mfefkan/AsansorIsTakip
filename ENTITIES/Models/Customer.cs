617e3ebfa2f8172234a0b34526516194f1d52d59
fatih efkan <mfefkan@hotmail.com>
2023-05-31 15:50:44 +0300

    Models'e başlandı.

---
+++ ENTITIES/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTITIES.Models
+{
+    public class Customer:BaseEntity
+    {
+        public string CustomerName { get; set; }
+        public string CustomerPhoneNumber { get; set; }
+        public string CustomerMailAddress { get; set; }
+    }
+}
